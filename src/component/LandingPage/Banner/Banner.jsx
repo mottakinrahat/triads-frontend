814ef3ce35ec8b5197cd785ca0daa6a4faf429f5
mottakinrahat <rahatchowdhury661@gmail.com>
2024-08-04 12:59:21 +0600

    marque

--- src/component/LandingPage/Banner/Banner.jsx
+++ src/component/LandingPage/Banner/Banner.jsx
@@ -6,6 +6,13 @@ const Banner = () => {
         <div>
             <ReuseMarquee text={'三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入 三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入'}
              gradient={true} speed={120} className="bg-[#4B0200] bg-shadow shadow-3xl text-8xl font-bold text-[#CC1414] w-full min-h-[130px] flex items-center" />
+
+
+            <ReuseMarquee text={'三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入 三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入'}
+             gradient={true} speed={120} className="bg-[#4B0200] bg-shadow shadow-3xl text-8xl font-bold text-[#CC1414] w-full min-h-[130px]
+              flex items-center" direction="right"/>
+
+            
         </div>
     );
 };
