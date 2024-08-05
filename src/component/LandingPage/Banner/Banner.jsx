1d65651f2d173171f91881225e4d3edb1f4518f5
mottakinrahat <rahatchowdhury661@gmail.com>
2024-08-05 20:09:08 +0600

    created hero section with symbol scale up down effect and button.

--- src/component/LandingPage/Banner/Banner.jsx
+++ src/component/LandingPage/Banner/Banner.jsx
@@ -4,11 +4,11 @@ import VerticalMarquee from '../../ReusableMarquee/VerticalMarquee';
 
 const Banner = () => {
     return (
-        <div className="relative  ">
+        <div className="relative  overflow-hidden">
             {/* Transparent overlay */}
             <div className="absolute inset-0 bg-[#4B0200] bg-opacity-80 z-10"></div>
            
-            <div className="relative z-20 col-span-8">
+            <div className="relative z-[300] col-span-8">
                 <ReuseMarquee 
                     gradient={true} 
                     speed={120} 
@@ -16,7 +16,7 @@ const Banner = () => {
                     三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入 三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入
                 </ReuseMarquee> 
 
-                <div className='pt-20 container'>
+                <div className='pt-20 container z-[200]'>
                     <div className='flex flex-col justify-center items-center gap-10'>
                         <img className='max-w-[600px]' src="/images/upBarWithText.png" alt="" />
                         <img className='max-w-[900px]' src="/images/striaosLogo.png" alt="" />
@@ -34,27 +34,57 @@ const Banner = () => {
                         </div>
                        <div className='flex items-center justify-center -mt-5'><button className='py-1 px-8 active:bg-black active:border-black active:text-white  border rounded-2xl text-lg font-bold border-[#880b16] bg-[#880b16]'>Copy</button></div>
                     </div>
+                    <div className='flex justify-center items-center my-12'>
+                        <a className='block p-5 bg-[#490004] text-[#d6010f] uppercase rounded-sm text-4xl font-bold border-[#d6010f] border-l-[1rem] border-r-[1rem] border-t-[0.15rem] border-b-[0.15rem]' href='#' target='_blank'>Follow The rio line</a>
+                    </div>
                 </div>
 
-                <VerticalMarquee>
-                    <div className='w-40 flex flex-col gap-10'>
-                        <img src="/images/striaosLogo.png" alt="" />
-                        <img src="/images/striaosLogo.png" alt="" />
-                        <img src="/images/striaosLogo.png" alt="" />
-                        <img src="/images/striaosLogo.png" alt="" />
-                        <img src="/images/striaosLogo.png" alt="" />
-                    </div>
-                </VerticalMarquee>
+
 
                 <ReuseMarquee 
                     gradient={true} 
                     speed={120} 
-                    className="bg-[#4B0200] bg-shadow shadow-3xl text-8xl font-bold text-[#CC1414] w-full h-[140px] flex items-center " 
+                    className="bg-[#4B0200] bg-shadow shadow-3xl text-8xl font-bold text-[#CC1414] w-full h-[140px] flex items-center z-[300]  " 
                     direction="right">
                     三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入 三合會正向你襲來 你無法逃脫 你無法隱藏 唯一的選擇就是加入
                 </ReuseMarquee>
             </div>
+            <div className='absolute top-0 left-[60px] max-w-[12%] w-full z-[200] '>
+            <div className='absolute z-20 w-full h-full animate-marquee '>
+                <div className='w-full space-y-4'>
+                {Array(33).fill().map((_, index) => (
+                    <img key={index} className='w-full h-auto object-contain animate-marquee' src="/images/striaosLogo.png" alt="" />
+                ))}
+                </div>
+                <div className='w-full space-y-4'>
+                {Array(33).fill().map((_, index) => (
+                    <img key={index} className='w-full h-auto object-contain' src="/images/striaosLogo.png" alt="" />
+                ))}
+                </div>
             
+            </div>
+            </div>
+
+            <div className='absolute top-0 right-[60px] max-w-[12%] w-full z-[200] '>
+            <div className='absolute z-20 w-full h-full animate-marquee '>
+                <div className='w-full space-y-4'>
+                {Array(33).fill().map((_, index) => (
+                    <img key={index} className='w-full h-auto object-contain animate-marquee' src="/images/striaosLogo.png" alt="" />
+                ))}
+                </div>
+                <div className='w-full space-y-4'>
+                {Array(33).fill().map((_, index) => (
+                    <img key={index} className='w-full h-auto object-contain' src="/images/striaosLogo.png" alt="" />
+                ))}
+                </div>
+            
+            </div>
+            </div>
+
+            <div className='absolute top-1/2 -translate-x-1/2 -translate-y-1/2 z-[100] left-1/2 '>
+                <img className='animate-scaleUpDown' src="/images/_symbol-f3ddca6a.png" alt="symbol image" />
+            </div>
+
         </div>
     );
 };
